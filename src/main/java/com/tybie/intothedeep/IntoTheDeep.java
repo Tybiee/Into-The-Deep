f45be16d93fea5021ea4009df7b00e7a907a9c67
Tybiee <tybiemeyer@gmail.com>
2022-11-09 00:06:26 -0500

    Initial File Setup

---
+++ src/main/java/com/tybie/intothedeep/IntoTheDeep.java
@@ -0,0 +1,14 @@
+package com.tybie.intothedeep;
+
+import net.fabricmc.api.ModInitializer;
+
+public class IntoTheDeep implements ModInitializer
+{
+    public static final String MOD_ID = "intothedeep";
+
+    @Override
+    public void onInitialize()
+    {
+        System.out.println("uwu");
+    }
+}
